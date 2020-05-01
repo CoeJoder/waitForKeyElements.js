406ff29e7e367f352e6ed61adea80e925a870528
CoeJoder <3125461+CoeJoder@users.noreply.github.com>
2020-05-01 15:32:20 -0700

    Modified script as described in the README doc.

--- waitForKeyElements.js
+++ waitForKeyElements.js
@@ -1,92 +1,53 @@
-/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
-    that detects and handles AJAXed content.
+/**
+ * A utility function for userscripts that detects and handled AJAXed content.
+ *
+ * Usage example:
+ *
+ *     function callback(domElement) {
+ *         domElement.innerHTML = "This text inserted by waitForKeyElements().";
+ *     }
+ * 
+ *     waitForKeyElements("div.comments", callback);
+ *     // or
+ *     waitForKeyElements(selectorFunction, callback);
+ *
+ * @param {(string|function)} selectorOrFunction - The selector string or function.
+ * @param {function} callback - The callback function; takes a single DOM element as parameter.
+ *                              If returns true, element will be processed again on subsequent iterations.
+ * @param {boolean} [waitOnce=true] - If true, only one iteration is performed.
+ * @param {number} [interval=300] - The time (ms) to wait between iterations.
+ */
+function waitForKeyElements(selectorOrFunction, callback, waitOnce, interval) {
+	if (typeof waitOnce === "undefined") {
+		waitOnce = true;
+	}
+	if (typeof interval === "undefined") {
+		interval = 300;
+	}
+	var targetNodes = (typeof selectorOrFunction === "function")
+			? selectorOrFunction()
+			: document.querySelectorAll(selectorOrFunction);
 
-    Usage example:
+	var targetsFound = targetNodes && targetNodes.length > 0;
+	if (targetsFound) {
+		targetNodes.forEach(function(targetNode) {
+			var attrAlreadyFound = "data-userscript-alreadyFound";
+			var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false;
+			if (!alreadyFound) {
+				var cancelFound = callback(targetNode);
+				if (cancelFound) {
+					targetsFound = false;
+				}
+				else {
+					targetNode.setAttribute(attrAlreadyFound, true);
+				}
+			}
+		});
+	}
 
-        waitForKeyElements (
-            "div.comments"
-            , commentCallbackFunction
-        );
-
-        //--- Page-specific function to do what we want when the node is found.
-        function commentCallbackFunction (jNode) {
-            jNode.text ("This comment changed by waitForKeyElements().");
-        }
-
-    IMPORTANT: This function requires your script to have loaded jQuery.
-*/
-function waitForKeyElements (
-    selectorTxt,    /* Required: The jQuery selector string that
-                        specifies the desired element(s).
-                    */
-    actionFunction, /* Required: The code to run when elements are
-                        found. It is passed a jNode to the matched
-                        element.
-                    */
-    bWaitOnce,      /* Optional: If false, will continue to scan for
-                        new elements even after the first match is
-                        found.
-                    */
-    iframeSelector  /* Optional: If set, identifies the iframe to
-                        search.
-                    */
-) {
-    var targetNodes, btargetsFound;
-
-    if (typeof iframeSelector == "undefined")
-        targetNodes     = $(selectorTxt);
-    else
-        targetNodes     = $(iframeSelector).contents ()
-                                           .find (selectorTxt);
-
-    if (targetNodes  &&  targetNodes.length > 0) {
-        btargetsFound   = true;
-        /*--- Found target node(s).  Go through each and act if they
-            are new.
-        */
-        targetNodes.each ( function () {
-            var jThis        = $(this);
-            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
-
-            if (!alreadyFound) {
-                //--- Call the payload function.
-                var cancelFound     = actionFunction (jThis);
-                if (cancelFound)
-                    btargetsFound   = false;
-                else
-                    jThis.data ('alreadyFound', true);
-            }
-        } );
-    }
-    else {
-        btargetsFound   = false;
-    }
-
-    //--- Get the timer-control variable for this selector.
-    var controlObj      = waitForKeyElements.controlObj  ||  {};
-    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
-    var timeControl     = controlObj [controlKey];
-
-    //--- Now set or clear the timer as appropriate.
-    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
-        //--- The only condition where we need to clear the timer.
-        clearInterval (timeControl);
-        delete controlObj [controlKey]
-    }
-    else {
-        //--- Set a timer, if needed.
-        if ( ! timeControl) {
-            timeControl = setInterval ( function () {
-                    waitForKeyElements (    selectorTxt,
-                                            actionFunction,
-                                            bWaitOnce,
-                                            iframeSelector
-                                        );
-                },
-                300
-            );
-            controlObj [controlKey] = timeControl;
-        }
-    }
-    waitForKeyElements.controlObj   = controlObj;
+	if (!targetsFound || !waitOnce) {
+		setTimeout(function() {
+			waitForKeyElements(selectorOrFunction, callback, waitOnce, interval);
+		}, interval);
+	}
 }
