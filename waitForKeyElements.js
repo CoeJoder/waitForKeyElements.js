ab3015ff372ca30afaec2557b8963ec786b43dac
CoeJoder <CoeJoder@users.noreply.github.com>
2020-09-29 19:23:38 -0700

    Convert tabs to spaces

--- waitForKeyElements.js
+++ waitForKeyElements.js
@@ -12,47 +12,47 @@
  *     waitForKeyElements(selectorFunction, callback);
  *
  * @param {(string|function)} selectorOrFunction - The selector string or function.
- * @param {function} callback - The callback function; takes a single DOM element as parameter.
- *                              If returns true, element will be processed again on subsequent iterations.
- * @param {boolean} [waitOnce=true] - Whether to stop after the first elements are found.
- * @param {number} [interval=300] - The time (ms) to wait between iterations.
- * @param {number} [maxIntervals=-1] - The max number of intervals to run (negative number for unlimited).
+ * @param {function}          callback           - The callback function; takes a single DOM element as parameter.
+ *                                                 If returns true, element will be processed again on subsequent iterations.
+ * @param {boolean}           [waitOnce=true]    - Whether to stop after the first elements are found.
+ * @param {number}            [interval=300]     - The time (ms) to wait between iterations.
+ * @param {number}            [maxIntervals=-1]  - The max number of intervals to run (negative number for unlimited).
  */
 function waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals) {
-	if (typeof waitOnce === "undefined") {
-		waitOnce = true;
-	}
-	if (typeof interval === "undefined") {
-		interval = 300;
-	}
-	if (typeof maxIntervals === "undefined") {
-		maxIntervals = -1;
-	}
-	var targetNodes = (typeof selectorOrFunction === "function")
-			? selectorOrFunction()
-			: document.querySelectorAll(selectorOrFunction);
+    if (typeof waitOnce === "undefined") {
+        waitOnce = true;
+    }
+    if (typeof interval === "undefined") {
+        interval = 300;
+    }
+    if (typeof maxIntervals === "undefined") {
+        maxIntervals = -1;
+    }
+    var targetNodes = (typeof selectorOrFunction === "function")
+            ? selectorOrFunction()
+            : document.querySelectorAll(selectorOrFunction);
 
-	var targetsFound = targetNodes && targetNodes.length > 0;
-	if (targetsFound) {
-		targetNodes.forEach(function(targetNode) {
-			var attrAlreadyFound = "data-userscript-alreadyFound";
-			var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false;
-			if (!alreadyFound) {
-				var cancelFound = callback(targetNode);
-				if (cancelFound) {
-					targetsFound = false;
-				}
-				else {
-					targetNode.setAttribute(attrAlreadyFound, true);
-				}
-			}
-		});
-	}
+    var targetsFound = targetNodes && targetNodes.length > 0;
+    if (targetsFound) {
+        targetNodes.forEach(function(targetNode) {
+            var attrAlreadyFound = "data-userscript-alreadyFound";
+            var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false;
+            if (!alreadyFound) {
+                var cancelFound = callback(targetNode);
+                if (cancelFound) {
+                    targetsFound = false;
+                }
+                else {
+                    targetNode.setAttribute(attrAlreadyFound, true);
+                }
+            }
+        });
+    }
 
-	if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
-		maxIntervals -= 1;
-		setTimeout(function() {
-			waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals);
-		}, interval);
-	}
+    if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
+        maxIntervals -= 1;
+        setTimeout(function() {
+            waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals);
+        }, interval);
+    }
 }
